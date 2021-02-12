ab92cd0bd16484e254510c7da0f663db8efd4d40
Aurelian Spodarec <aurelianxspodarec@gmail.com>
2021-02-12 23:58:26 +0000

    Added blog exerpt

---
+++ themes/bureau-of-builders/resources/views/components/blog-excerpt.blade.php
@@ -0,0 +1,23 @@
+<article id="">
+
+    <div>
+    <a href="">
+        <img src="" />
+    </a>
+    </div>
+
+    <div>
+        <h4>Title</h4>
+        <div>
+            <p>February 11, 2021</p>
+            <p>Programming</p>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto consequuntur dolore, doloremque fuga illo modi odit perspiciatis quia quidem sit. Accusantium dolores error nam perferendis perspiciatis quas saepe sapiente! Quidem?
+        </p>
+
+        <a href="">Read More</a>
+    </div>
+
+
+</article>
